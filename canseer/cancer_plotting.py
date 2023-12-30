21170a63a94fca354546adff3765da0a951ed15e
Pav Loginovic <p.loginovic@gmail.com>
2023-12-30 19:02:45 +0200

    Added breaches_animated_plot() function, a plotly interactive plot

--- canseer/cancer_plotting.py
+++ canseer/cancer_plotting.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from data_wrangling import select_data
 
 def plot_stacked(data, labels, y_label, leg_loc='best', n_cols=1):
     """
@@ -45,4 +47,72 @@ def plot_stacked(data, labels, y_label, leg_loc='best', n_cols=1):
     ax.legend(loc=leg_loc, ncol=n_cols)
             
     return fig, ax
+
+
+# ToDo for the functions below: display x-axis labels/ticks – currently not working/adding too many 
+# Add appropriate titles
+# Add national averages to compare with
+# Round the proportions displayed
+def breaches_animated_plot(data, filters, window_size=5):
+    """
+    Create an animated plot with a moving average.
+
+    Parameters:
+    - data (pd.DataFrame): The input DataFrame containing the relevant columns.
+    - filters (list): A list of tuples specifying the filters to be applied to the data.
+    - window_size (int): The size of the moving average window.
+
+    Returns:
+    - None: The plot is displayed interactively.
+    """
+
+    # Apply filters to the data
+    for filter_key, filter_value in filters:
+        data = select_data(data, filters)
+
+    # Sort data by the 'PERIOD' column
+    data.sort_values(by='PERIOD', inplace=True)
+
+    # Create the interactive graph
+    fig = go.Figure()
+
+    # Add a scatter plot for the proportion of breaches
+    fig.add_trace(go.Scatter(x=data['PERIOD'], y=data['PROPORTION_BREACHES'], mode='lines+markers', name='Proportion of Breaches'))
+
+    # Add a line plot for the moving average
+    fig.add_trace(go.Scatter(x=data['PERIOD'], y=data['MOVING_AVERAGE'], mode='lines', name=f'Moving Average (Window={window_size})'))
+
+    # Customize the layout
+    fig.update_layout(
+        title='Proportion of Breaches Over Time',
+        xaxis_title='Period',
+        yaxis_title='Proportion of Breaches',
+        hovermode='x',
+        xaxis=dict(tickmode='array',
+                   tickvals=data['PERIOD'].astype(int) / 10**9,  # Convert datetime to timestamp in seconds
+                   ticktext=data['PERIOD'].dt.strftime('%b %Y'),  # Display months and years as tick labels
+                   ),
+        updatemenus=[dict(
+            type='buttons',
+            showactive=False,
+            buttons=[dict(
+                label='Play',
+                method='animate',
+                args=[None, dict(frame=dict(duration=500, redraw=True), fromcurrent=True)])
+            ])
+        ])
+
+    # Create animation frames
+    frames = [go.Frame(data=[go.Scatter(x=data['PERIOD'].iloc[:i + 1],
+                                        y=data['MOVING_AVERAGE'].iloc[:i + 1],
+                                        mode='lines',
+                                        marker=dict(color='red'),
+                                        name=f'Moving Average (Window={window_size})')]) for i in range(len(data))]
+
+    # Add frames to the figure
+    fig.frames = frames
+
+    # Show the interactive plot
+    fig.show()
+
 
