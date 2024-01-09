93d7a9b1f95f77eb7025ce78c159032c14067748
chowienczyk <152490347+chowienczyk@users.noreply.github.com>
2024-01-09 19:06:13 +0000

    Update data_wrangling.py

--- canseer/data_wrangling.py
+++ canseer/data_wrangling.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-import pandas as pd
-import numpy as np
-
 def get_provider_data():
     """
     Returns the provider dataframe
