7c83ed56371f68d09eba37b9b92656137296e826
Pav Loginovic <p.loginovic@gmail.com>
2024-01-12 00:53:50 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canseer",
-    version="0.1.0",
+    version="0.1.2",
     description="A Python package simplifying workflow with NHS Cancer data",
     packages=find_packages(),
     classifiers=[
