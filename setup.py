87ebc915adfe1db3b4d4046dbaeca54c268dc1e3
Pav Loginovic <p.loginovic@gmail.com>
2024-01-11 23:55:13 +0200

    added toml file for PyPI

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="canseer",
+    version="0.1.0",
+    description="A Python package simplifying workflow with NHS Cancer data",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Healthcare/Medicine",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+    url = "https://github.com/ploginovic/hpdm139_A2"
+)
